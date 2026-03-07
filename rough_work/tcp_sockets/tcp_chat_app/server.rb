fd3049bbbe4886ca409a328e24f37d95aac9badc
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2026-03-07 16:35:31 -0500

    Develop ideas for server

--- rough_work/tcp_sockets/tcp_chat_app/server.rb
+++ rough_work/tcp_sockets/tcp_chat_app/server.rb
@@ -9,29 +9,47 @@ module TCPChatApp
     SERVER_PORT = 2211
     SERVER_HOST = '0.0.0.0'
     MAX_BACKLOG_SIZE = 100
-    LOG_STREAM = $stdout
+    LOG_FILE = $stdout
 
     def initialize
+      @waiting_queue = []
       @listening_socket = Socket.new(:INET, :STREAM).then do |sock|
         addr = Socket.pack_sockaddr_in(SERVER_PORT, SERVER_HOST)
         sock.bind addr
         sock.listen(MAX_BACKLOG_SIZE)
         sock
       end
-      LOG_STREAM.puts "Server listening on #{SERVER_PORT} on all interfaces"
-
-      @peer_map = {}
+      LOG_FILE.puts "{#{Time.now.utc}}: Server listening on #{SERVER_PORT} on all interfaces"
     end
 
     def run
-      raise NotImplementedError
-      # PROGRAM FLOW:
-      # - listen for incoming connections
-      # - when a client connects, push them onto a queue
-      # - when there are at least 2 people in the queue, pop the Q twice and 
-      # put them in a chatroom
-      # - execute 
+      loop do
+        # 1. Match people in the waiting queue, put them into a room
+        # 2. select() for sockets
+        # 3. Handle new connections
+        # 4. Handle readable sockets
+        # 5. Handle writable sockets
+        read_ready, write_ready= IO.select([@listening_socket] + @peer_map.keys, @peer_map.keys)
+
+        read_ready.each do |sock|
+          if sock == @listening_socket
+            # When client connects, they are put into a "waiting room" queue
+            # when there are >= 2 people in the queue, will start to match them
+          else
+            # read
+          end
+        end
+      end
     end
   end
 
+end
+
+module TCPChatApp
+  # Wraps all Socket logic, connection handling, etc.
+  class Connection
+    def initialize
+
+    end
+  end
 end
