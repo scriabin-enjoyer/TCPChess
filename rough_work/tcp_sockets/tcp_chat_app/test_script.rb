c39f03586952b88b266469a94ab72b7fd3950565
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2026-03-08 11:27:50 -0400

    Add test script driver

--- rough_work/tcp_sockets/tcp_chat_app/test_script.rb
+++ rough_work/tcp_sockets/tcp_chat_app/test_script.rb
@@ -4,3 +4,11 @@ require_relative 'server'
 require_relative 'client'
 require_relative 'message'
 require_relative 'ui'
+
+test_function = ARGV.first.downcase
+
+if test_function == 'server'
+  TCPChatApp::Server.new.run
+elsif test_function == 'client'
+  client = TCPChatApp::Client.new
+end
