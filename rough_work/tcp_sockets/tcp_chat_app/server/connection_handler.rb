3122762262365f704565a7c77ff88ee15bc8652a
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2026-03-12 18:37:42 -0400

    Update Interface

--- rough_work/tcp_sockets/tcp_chat_app/server/connection_handler.rb
+++ rough_work/tcp_sockets/tcp_chat_app/server/connection_handler.rb
@@ -6,7 +6,14 @@ module TCPChatApp
       raise NotImplementedError
     end
 
+    # Delegate action based on readable connection
+    # Dispatched from Server
     def process_readable(connection)
+      raise NotImplementedError
+    end
+
+    # Receive new ChatRoom instance from AcceptHandler
+    def receive(new_chat_room)
       raise NotImplementedError
     end
   end
