37d3aef079e6303000001c5ec7d9420d25248829
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2026-03-12 18:32:34 -0400

    Implement first decent iteration of reactor

--- rough_work/tcp_sockets/tcp_chat_app/server/server.rb
+++ rough_work/tcp_sockets/tcp_chat_app/server/server.rb
@@ -2,6 +2,10 @@
 
 require 'socket'
 
+require_relative 'connection'
+require_relative 'accept_handler'
+require_relative 'connection_handler'
+
 module TCPChatApp
   # Implements a single-threaded, event-driven server that maintains a list of
   # chatrooms within which clients can send each other messages.
@@ -10,11 +14,12 @@ module TCPChatApp
     SERVER_HOST = '0.0.0.0'
     MAX_BACKLOG_SIZE = 100
     LOG_FILE = $stdout
+    MAX_CLIENTS = 1000
 
     def initialize
-      @intake_queue = IntakeQueue.new
-      @clients = []
-      # Expose socket creation process just to be explicit
+      @active_client_handles = {}
+      @connection_handler = ConnectionHandler.new
+      @accept_handler = AcceptHandler.new(@connection_handler)
       @listening_socket = Socket.new(:INET, :STREAM).then do |sock|
         addr = Socket.pack_sockaddr_in(SERVER_PORT, SERVER_HOST)
         sock.bind addr
@@ -24,81 +29,32 @@ module TCPChatApp
       LOG_FILE.puts "{#{Time.now.utc}}: Server listening on #{SERVER_PORT} on all interfaces"
     end
 
-    # NOTE: WIP
     def run
       loop do
-        # TODO: Need to also define hooks/callbacks for connections to register
-        # their interest in being writable — in the case that the server needs
-        # to relay messages, or ping clients, etc.
-        readable, writable = IO.select([@listening_socket] + @clients, @clients)
-        readable.each { |conn| handle_readable(conn) }
-        writable.each { |conn| handle_writable(conn) }
+        to_read = @active_client_handles.values.select(&:monitor_for_reading?) + @listening_socket
+        to_write = @active_client_handles.values.select(&:monitor_for_writing?)
+
+        readables, writables = IO.select(to_read, to_write)
+        readables.each { |conn| handle_readable(conn) }
+        writables.each { |conn| handle_writable(conn) }
       end
     end
 
-    # NOTE: WIP
     def handle_readable(connection)
       if connection == @listening_socket
-        # In the very unlikely case that the listening_socket starts filling up
-        # its backlog, we should pop the backlog until its empty
-        while (new_client = connection.accept_nonblock(exception: false)) != :wait_readable
-          @intake_queue.process new_client
+        loop do
+          new_client = connection.accept_nonblock(exception: false)
+          break if new_client == :wait_readable
+
+          @active_client_handles[new_client.fileno] = Connection.new(new_client)
+          @accept_handler.intake(new_client)
         end
       else
-        handle_client_event(connection)
+        @connection_handler.process_readable(connection)
       end
-    end
-
-    # NOTE: WIP
-    def handle_client_event(connection)
-      raise NotImplementedError
     end
 
     def handle_writable(connection)
-      raise NotImplementedError
-    end
-  end
-
-  # Represents a connection between the Client and this Server
-  # Wraps all Socket logic, connection handling, etc.
-  # Manages all low-level socket input, output, and life-cycle
-  # Acts as a protocol bridge as well, should handle streaming data from
-  # transport layer and reconstructing full application-level messages,
-  # especially when the message boundaries are not preserved with partial reads
-  class Connection
-    def initialize(socket)
-      raise NotImplementedError
-    end
-
-    def to_io
-      raise NotImplementedError
-    end
-  end
-
-  # Represents a chat room between 2 clients (Connection objects)
-  class ChatRoom
-  end
-
-  # Represents 
-  class RoomManager
-  end
-
-  # Processes newly accepted client sockets and matches them with another
-  # waiting client on a FIFO basis. 
-  class IntakeQueue
-    def initialize
-      @queue = []
-    end
-
-    def process(conn)
-      @queue << conn
-    end
-
-    def match
-      raise NotImplementedError
-    end
-
-    def ready_to_match
       raise NotImplementedError
     end
   end
