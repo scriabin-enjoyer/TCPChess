3df21d8243c4ae2300ecc0bef7ea13957d31e81a
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2026-03-13 00:25:10 -0400

    Add ChatRoom and RoomManager

---
+++ rough_work/tcp_sockets/tcp_chat_app/server/chat_room.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module TCPChatApp
+  # Consists of 2 clients
+  # Responsible for handling all state and communication between 2 connected
+  # clients
+  class ChatRoom
+    def initialize(client1, client2)
+      @client1 = client1
+      @client2 = client2
+    end
+
+    # 
+  end
+end
