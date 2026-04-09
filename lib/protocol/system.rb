c0d2c24dc2284a75b0c05aa508163e90c1af649d
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2026-04-09 17:21:45 -0400

    Protocol: add empty system and chess modules

---
+++ lib/protocol/system.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module MyGameServer
+  module Protocol
+    # Implements the SYSTEM-type protocol messages
+    module System
+      # echo req, reply
+      # ping pong
+      # bye
+      # join game, queued, join success
+      # game start
+      # game end
+      # game disconnect
+      # Leave game
+    end
+  end
+end
