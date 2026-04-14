c34a35a5c4bf6f004183e30d6f167fce1f08aa89
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2026-04-14 13:17:20 -0400

    Add protocol test files

---
+++ test/protocol/protocol_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../test_protocol_helper'
+
+describe MyGameServer::Protocol do
+  describe ".parse_tlv" do
+    describe "when data is good" do
+      it "works" do
+
+      end
+    end
+  end
+
+  describe ".serialize_tlv" do
+  end
+
+  describe "helpers" do
+  end
+end
---
+++ test/test_protocol_helper.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+
+require_relative '../lib/protocol/main'
+
+module ProtocolHelper
+  def generate
+  end
+end
